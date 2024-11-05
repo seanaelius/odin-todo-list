f26a97c71b2e5a2ff96911f8805b9daa022d217c
seanaelius <sdc.startup@gmail.com>
2024-11-05 18:40:28 -0500

    Finalize create_element function in create-element.js Test create-element in index.js

---
+++ src/components/create-element.js
@@ -0,0 +1,17 @@
+export {create_element}
+
+function create_element(type, classList, text) {
+    //########################### CREATE ELEMENT ###########################
+    var ele = document.createElement(`${type}`)
+
+    //########################### ADD CLASS NAMES ###########################
+    if ( classList != null ) {
+        for (let className in classList){
+            ele.classList.add(classList[className]);
+        }
+    }
+    //########################### ADD TEXT TO ELEMENT ###########################
+    if (text != null) ele.textContent = text
+
+    return ele;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css"
+
+import { create_element } from "./components/create-element"
+
+//########################### TEST CREATE H1 w/ CLASS LIST AND TEXT ###########################
+const content = document.querySelector("#content")
+content.appendChild(create_element("h1",['introduction','title'],"Hello"))
